d3b9918b182eaa40b0e7d269c4c47b89bc5cdcb8
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-16 21:26:38 +0300

    task 2: done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, UserLoginView, UserLogoutView, UserRegisterView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,11 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def list_books(request):
     """
@@ -18,4 +23,24 @@ class LibraryDetailView(DetailView, ListView):
     """
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library' 
+    context_object_name = 'library'
+
+# User authentication views
+
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('list_books')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        from django.contrib.auth import login
+        login(self.request, self.object)  # Automatically log in new user
+        return response
