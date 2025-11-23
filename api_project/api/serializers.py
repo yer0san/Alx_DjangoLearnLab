a8a6c7e8aa46944b6d61f041cc1f320ced1c6607
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-23 19:20:33 +0300

    import name change

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fiels = '__all__'
