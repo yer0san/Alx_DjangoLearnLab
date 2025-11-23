824d456c1121fd9e7f90e2e0933532f1a5d49558
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-23 19:42:37 +0300

    week 12: task 2 done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
 
@@ -6,5 +7,8 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
