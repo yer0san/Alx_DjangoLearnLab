95bf29f8479d13567a014cd7b5612caa4dac56f8
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-23 19:22:28 +0300

    alx sucks

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
