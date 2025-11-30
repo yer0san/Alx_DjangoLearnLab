332b2d105e1019d54f1cd3dfa27c03cf805839e8
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-30 11:10:29 +0300

    mtsmm

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Book, Author
 from datetime import datetime
 
-class BookSerializer(serializers.ModelSerializerSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
