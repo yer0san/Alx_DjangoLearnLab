832f9f74de7c8879bda7625a906e7778a72658cc
Yerosan Bekele <lebrondee20@gmail.com>
2025-11-30 13:54:00 +0300

    week 13 task1 done

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = []
+    filterset_fields = ["author", "publication_year"]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def perform_create(self, serializer):
+        book = serializer.save()
+
+        if book.publication_year < 1500:
+            book.delete()
+            raise ValidationError("Books earlier than year 1500 are not allowed.")
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        book = serializer.save()
+
+        if book.title.strip() == "":
+            raise ValidationError("Title cannot be empty.")
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
