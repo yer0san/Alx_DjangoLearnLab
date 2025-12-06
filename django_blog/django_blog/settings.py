fafbcbb9529040667baac9b372899109786fc8ad
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-06 09:29:42 +0300

    update: settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USER = "dee"
+PORT = 8000
