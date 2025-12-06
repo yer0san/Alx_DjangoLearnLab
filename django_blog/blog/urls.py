e6c9425b5a221864eed6e3a900318db211bd233d
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-06 15:47:17 +0300

    minor change, why are you reading this? get a life dude

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
 ]
