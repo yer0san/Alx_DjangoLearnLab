a3eb3a3c4f488c906db9a6367d396ed8a5b5d70d
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-06 17:24:47 +0300

    almost done, i'm about to lose my mind

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
-    path("post/<int:pk>/comment/new/", views.CommentCreateView.as_view(), name="comment-create"),
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
