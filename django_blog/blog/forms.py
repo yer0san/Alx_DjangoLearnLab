fd22056dd2acc1fbec7f7de52e4dc942a76a54ed
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-07 14:42:55 +0300

    apparently it wanted me to leave the tagWidget empty

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -34,5 +34,5 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ["title", "content", "tags"]
         widgets = {
-            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+            'tags': TagWidget(),
         }
