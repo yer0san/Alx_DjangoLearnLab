5999ed2449a27d58b1522978747ec2eac6703a06
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-11 17:31:40 +0300

    dumb stuff

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
