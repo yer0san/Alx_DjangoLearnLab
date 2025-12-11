78a541cbf1ec994053325036bf3aade73700ae4c
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-12 00:48:08 +0300

    update routing name

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
 ]
 
 if settings.DEBUG:
