83f4b4c55837cc16a31c7e8791693a85df3640f7
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-12 16:27:57 +0300

    test three check

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,8 +34,8 @@ class FeedView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
